40b911558f06e4c6a756fbe917fadec27ac87b98
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2023-01-24 12:07:31 -0600

    Added to easy slidingWindow

---
+++ easy/slidingWindow/maxSumSubArray/maxSumSubArray.js
@@ -0,0 +1,19 @@
+function maxSumSubarray(arr, windowSize) {
+    let maxSum = 0;
+    let windowSum = 0;
+    let start = 0;
+    let end = 0;
+    while (end < arr.length) {
+      windowSum += arr[end]; // adding the element to the window
+      if (end >= windowSize - 1) {
+        maxSum = Math.max(maxSum, windowSum); // update maxSum if the current window sum is greater
+        windowSum -= arr[start]; // remove the start element from the window
+        start++; // move the start pointer to the next element
+      }
+      end++; // move the end pointer to the next element
+    }
+    return maxSum;
+  }
+  
+  
+  
